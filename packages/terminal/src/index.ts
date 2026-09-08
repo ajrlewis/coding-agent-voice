5724eea33d00765e75c6391b99d8a0c23afa27be
ajrlewis <hello@ajrlewis.com>
2026-09-08 13:18:51 +0100

    feat(terminal): manage interactive PTY lifecycle

---
+++ packages/terminal/src/index.ts
@@ -0,0 +1,12 @@
+export {
+  findExecutable,
+  NodePtyTerminal,
+  TerminalProcessError,
+  type Disposable,
+  type NodePtyTerminalOptions,
+  type PtyFactory,
+  type PtyProcess,
+  type TerminalHost,
+  type TerminalRunner,
+  type TerminalRunRequest,
+} from "./terminal.js";
