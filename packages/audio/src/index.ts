fc444e3ca3808789f0df207b2c4b4bbd9d3c30fa
ajrlewis <hello@ajrlewis.com>
2026-09-08 11:54:51 +0100

    feat(audio): add replaceable FFmpeg recorder

---
+++ packages/audio/src/index.ts
@@ -0,0 +1,12 @@
+export {
+  AudioRecordingError,
+  FfmpegRecorder,
+  buildFfmpegInputArgs,
+  type AudioBackend,
+  type FfmpegRecorderOptions,
+} from "./ffmpeg.js";
+export type {
+  AudioRecorder,
+  RecordedAudio,
+  RecordingSession,
+} from "./recorder.js";
